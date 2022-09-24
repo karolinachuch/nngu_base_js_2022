705968284237207f4355d433b5c0916949e07951
karolinchuch <kat.chuvatkina@yandex.ru>
2022-09-24 10:37:32 +0300

    Добавлены файлы первого занятия

---
+++ Index.js
@@ -0,0 +1,15 @@
+console.log ("Hello world!");
+const namePlanet3 ="Earth";
+console.log (namePlanet3);
+let currentUserTime ="Time";
+console.log (currentUserTime);
+let statmentsCount ="Statments number";
+console.log (statmentsCount);
+let isCash ="Cash or card";
+console.log (isCash);
+const firstName ="First Name";
+console.log (firstName);
+const secondName ="Second Name";
+console.log (secondName);
+const lastName ="Last Name";8
+console.log (lastName);
